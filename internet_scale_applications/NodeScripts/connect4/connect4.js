00d76f5c771ba9a77da681d0216e51a06fc0b661
wloving77 <108347120+wloving77@users.noreply.github.com>
2023-09-22 14:33:39 -0400

    quiz2 comm

--- internet_scale_applications/NodeScripts/connect4/connect4.js
+++ internet_scale_applications/NodeScripts/connect4/connect4.js
@@ -1,4 +1,49 @@
-/* Connect4 Logic Below: */
+
+
+
+/*
+Server Logic:
+*/
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const server = http.createServer();
+
+server.on("request", (request, response) => {
+
+    //parse url and split on /
+    const parsedUrl = url.parse(request.url);
+    const path = parsedUrl.pathname;
+    const pathParts = path.split("/");
+
+    // load the connect4 html if that path is specified, with error handling
+    if (pathParts[1] == "connect4.html") {
+        fs.readFile("connect4/html/connect4.html", (err, data) => {
+            if (err) {
+                response.writeHead(404, { "Content-type": "text/plain" });
+                response.end("File not found");
+            } else {
+                response.writeHead(200, { "Content-type": "text/html" });
+                response.end(data);
+            }
+        });
+    } else {
+        response.writeHead(404, { "Content-type": "text/plain" });
+        response.end("Invalid Url");
+    }
+
+})
+
+server.on("error'", error => console.error(error.stack));
+
+server.listen(3000, "127.0.0.1", () => console.log("Server Running at http://localhost:3000"));
+
+
+/*
+Connect4 Logic: 
+*/
 
 class Player {
     constructor(playerName, playerNumber) {
